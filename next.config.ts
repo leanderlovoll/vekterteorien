697410ab36fa35f753bad2aabf7adae7305b071a
leanderlovoll <leanderlovoll11@gmail.com>
2026-02-20 23:18:27 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/vektorteorien",
-  assetPrefix: "/vektorteorien/",
-  trailingSlash: true,
-  images: { unoptimized: true },
-};
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
+const nextConfig: NextConfig = isGithubPages
+  ? {
+      output: "export",
+      basePath: "/vekterteorien",
+      assetPrefix: "/vekterteorien/",
+      trailingSlash: true,
+      images: { unoptimized: true },
+    }
+  : {
+      // Normal Vercel deploy
+      images: { unoptimized: false },
+    };
 
 export default nextConfig;
