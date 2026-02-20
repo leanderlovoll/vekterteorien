a3a9492b7aca8a3925713c247b934da82cc88b0f
leanderlovoll <leanderlovoll11@gmail.com>
2026-02-20 22:50:03 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/vektorteorien",
-  assetPrefix: "/vektorteorien/",
+  basePath: "/vekterteorien",
+  assetPrefix: "/vekterteorien/",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
